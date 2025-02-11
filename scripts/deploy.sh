5fc30be3aa4ec40d0697cf1084471831acc12999
mroluwasesan <mr.oluwasesan@gmail.com>
2025-02-11 21:43:55 +0100

    update

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 set -e
 
-# Pull latest code
-cd /home/${DEPLOY_USER}/fastapi-book-project
+# Set the project directory
+PROJECT_DIR="/home/${DEPLOY_USER}/fastapi-book-project"
+
+# Create the project directory if it doesn't exist
+mkdir -p "$PROJECT_DIR"
+
+# Change to the project directory
+cd "$PROJECT_DIR"
+
+# Clone the repository if it doesn't exist
+if [ ! -d .git ]; then
+  git clone https://github.com/mroluwasesan/fastapi-book-project.git .
+fi
+
+# Pull the latest code
 git pull origin main
 
 # Build and restart Docker container
